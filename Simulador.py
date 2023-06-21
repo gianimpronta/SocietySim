65c2ce05978883779891afab41eabcd13074b0b4
Gianpaolo Martins Impronta <gian.impronta@gmail.com>
2023-06-21 07:09:00 -0300

    Create Simulador.py

---
+++ Simulador.py
@@ -0,0 +1,22 @@
+import time
+
+class Simulador:
+    def __init__(self, mundo, duracao, passo_tempo):
+        self.mundo = mundo
+        self.duracao = duracao
+        self.passo_tempo = passo_tempo
+
+    def iniciar(self):
+        for t in range(0, self.duracao, self.passo_tempo):
+            print(f"Dia {t}")
+            
+            # Atualiza o mundo
+            self.mundo.atualizar()
+
+            # Aqui você pode inserir outras funcionalidades, como a geração de eventos, a migração de pessoas, etc.
+            
+            time.sleep(self.passo_tempo)
+
+    def finalizar(self):
+        print("A simulação foi finalizada.")
+        # Aqui você pode adicionar qualquer lógica que queira executar no final da simulação, como salvar os resultados, exibir gráficos, etc.
